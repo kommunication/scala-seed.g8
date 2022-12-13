6dba3bfaf246b5dccaceb2f38219df11ac40168b
Komlan Egoh <komlan.egoh@alteryx.com>
2022-12-13 17:15:28 -0500

    minor changes

--- src/main/g8/src/main/scala/$package__lower,package,packaged$/$name__lower,word$/Main.scala
+++ src/main/g8/src/main/scala/$package__lower,package,packaged$/$name__lower,word$/Main.scala
@@ -2,9 +2,9 @@ package $package;format="lower,package"$
 package $name;format="lower,word"$
 
 object Main extends App {
-  println("─" * 100)
+  println("─" * 50)
 
   println("hello world")
 
-  println("─" * 100)
+  println("─" * 50)
 }
